a6abddf9224b09b20547cb52b61be659e5860394
Brayden <brayden.jo@outlook.com>
2020-09-23 11:56:27 +0900

    import basic project

---
+++ ch07/dataflow_ouput_for/tb/main.cc
@@ -0,0 +1,58 @@
+#include <hls_stream.h>
+#include <iostream>
+#include "../hls/dut.h"
+
+using namespace std;
+
+int main(void) 
+{
+    hls::stream<pix_unit_t>         chan_in;
+    hls::stream<pix_unit_t>         chan_out_b;
+    hls::stream<pix_unit_t>         chan_out;
+    uint8                           mem[2][8][8];       // two 8x8 blocks
+
+    // feeding 
+    for (int block=0; block < 1; block++) {
+        for (int row=0; row < 8; row++) {
+            pix_unit_t unit;
+
+            // fill the row
+            for (int col=0; col < 8; col++) {
+                unit.data[col] = 64 * block + row * 8 + col;
+            }
+
+            chan_in.write(unit);
+        }
+    }
+
+    // call DUT
+    dut(
+        // IN
+        chan_in,
+        // OUT
+        chan_out_b,
+        chan_out 
+    );
+
+    while (!chan_out_b.empty()) {
+        pix_unit_t unit = chan_out_b.read();
+
+        for (int i =0; i < 8; i++) {
+            cout.width(3);
+            cout << unit.data[i] << " "; 
+        }
+        cout << endl;
+    }
+
+    while (!chan_out.empty()) {
+        pix_unit_t unit = chan_out.read();
+
+        for (int i =0; i < 8; i++) {
+            cout.width(3);
+            cout << unit.data[i] << " "; 
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
